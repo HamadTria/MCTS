ea0c5ede5a339b52e5228a804ce27396e44425ed
HamadTria <triahamad@hotmail.com>
2024-04-19 20:22:37 +0200

    added a play again and exit buttons

--- tictactoe pygame.py
+++ tictactoe pygame.py
@@ -12,6 +12,11 @@ PLAYER_X_COLOR = (255, 0, 0)
 PLAYER_O_COLOR = (0, 0, 255)
 FONT_COLOR = (0, 0, 0)
 FONT_SIZE = 50
+BUTTON_COLOR = (100, 100, 100)
+BUTTON_FONT_COLOR = (255, 255, 255)
+BUTTON_FONT_SIZE = 20
+BUTTON_WIDTH = 100
+BUTTON_HEIGHT = 50
 
 class TicTacToe():
     def __init__(self, board=None):
@@ -67,6 +72,7 @@ class TicTacToe():
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption("Tic Tac Toe")
         font = pygame.font.SysFont(None, FONT_SIZE)
+        button_font = pygame.font.SysFont(None, BUTTON_FONT_SIZE)
         clock = pygame.time.Clock()
 
         mcts = MCTS()
@@ -87,13 +93,11 @@ class TicTacToe():
 
                     self = self.make_move(move)
                     if self.is_win():
-                        print('Player "%s" has won the game!\n' % self.player_2)
-                        pygame.quit()
-                        sys.exit()
+                        self.end_game_screen(screen, font, button_font, "Player '%s' has won!" % self.player_2)
+                        return
                     elif self.is_draw():
-                        print('Game is drawn!\n')
-                        pygame.quit()
-                        sys.exit()
+                        self.end_game_screen(screen, font, button_font, "Game is drawn!")
+                        return
 
                     best_move = mcts.search(self, MAX_ITER)
                     try:
@@ -126,6 +130,41 @@ class TicTacToe():
 
             text_rect = text.get_rect(center=(x, y))
             screen.blit(text, text_rect)
+
+    def end_game_screen(self, screen, font, button_font, message):
+        screen.fill(BG_COLOR)
+        text = font.render(message, True, FONT_COLOR)
+        text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - FONT_SIZE))
+        screen.blit(text, text_rect)
+
+        play_again_button_rect = pygame.Rect((SCREEN_WIDTH // 2 - BUTTON_WIDTH // 2, SCREEN_HEIGHT // 2), (BUTTON_WIDTH, BUTTON_HEIGHT))
+        exit_button_rect = pygame.Rect((SCREEN_WIDTH // 2 - BUTTON_WIDTH // 2, SCREEN_HEIGHT // 2 + BUTTON_HEIGHT + 10), (BUTTON_WIDTH, BUTTON_HEIGHT))
+
+        pygame.draw.rect(screen, BUTTON_COLOR, play_again_button_rect)
+        pygame.draw.rect(screen, BUTTON_COLOR, exit_button_rect)
+
+        play_again_text = button_font.render("Play Again", True, BUTTON_FONT_COLOR)
+        play_again_text_rect = play_again_text.get_rect(center=play_again_button_rect.center)
+        screen.blit(play_again_text, play_again_text_rect)
+
+        exit_text = button_font.render("Exit", True, BUTTON_FONT_COLOR)
+        exit_text_rect = exit_text.get_rect(center=exit_button_rect.center)
+        screen.blit(exit_text, exit_text_rect)
+
+        pygame.display.flip()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if play_again_button_rect.collidepoint(event.pos):
+                        self.__init__()
+                        self.game_loop()
+                    elif exit_button_rect.collidepoint(event.pos):
+                        pygame.quit()
+                        sys.exit()
 
 if __name__ == '__main__':
     board = TicTacToe()
